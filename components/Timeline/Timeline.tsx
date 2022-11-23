c2373b28252cdfdbf755a773b826e6faec9a2bec
nguyentan2212 <nguyentan2212@gmail.com>
2022-11-23 13:48:01 +0700

    change timeline section 's background

--- components/Timeline/Timeline.tsx
+++ components/Timeline/Timeline.tsx
@@ -5,29 +5,31 @@ type Props = {};
 
 const Timeline = (props: Props) => {
   return (
-    <div className="container py-20 text-3xl flex flex-col">
-      <h2 className="section-title">My Experience</h2>
-      <div className="grid grid-cols-2 gap-8">
-        <TimelineItem
-          title="Computer Science"
-          subtitle="Stanford University - 2003"
-          desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
-        />
-        <TimelineItem
-          title="Computer Science"
-          subtitle="Stanford University - 2003"
-          desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
-        />
-        <TimelineItem
-          title="Computer Science"
-          subtitle="Stanford University - 2003"
-          desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
-        />
-        <TimelineItem
-          title="Computer Science"
-          subtitle="Stanford University - 2003"
-          desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
-        />
+    <div className="bg-[#fbf8f8] w-full">
+      <div className="container py-20 text-3xl flex flex-col">
+        <h2 className="section-title">My Experience</h2>
+        <div className="grid grid-cols-2 gap-8">
+          <TimelineItem
+            title="Computer Science"
+            subtitle="Stanford University - 2003"
+            desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
+          />
+          <TimelineItem
+            title="Computer Science"
+            subtitle="Stanford University - 2003"
+            desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
+          />
+          <TimelineItem
+            title="Computer Science"
+            subtitle="Stanford University - 2003"
+            desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
+          />
+          <TimelineItem
+            title="Computer Science"
+            subtitle="Stanford University - 2003"
+            desc="Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut."
+          />
+        </div>
       </div>
     </div>
   );
