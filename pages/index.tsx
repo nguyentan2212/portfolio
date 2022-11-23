e65e2f99c0ca218ce60b6437f308560e03b74d06
nguyentan <nguyentan@LAPTOP-PUU745D2>
2022-11-23 12:26:20 +0700

    add hire me form into page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
-import ExperienceList from "../components/ExperienceList";
+import Timeline from "../components/Timeline";
+import HireMeForm from "../components/HireMeForm";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <div className="w-full h-[1px] bg-slate-200"></div>
       <Skills />
-      <ExperienceList />
+      <div className="w-full h-[1px] bg-slate-200"></div>
+      <Timeline />
+      <div className="w-full h-[1px] bg-slate-200"></div>
+      <HireMeForm />
+      <div className="w-full h-[1px] bg-slate-200"></div>
       <footer></footer>
     </div>
   );
