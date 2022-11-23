e1cc51c9ce0ad4316c7ca13842fa69a717368206
nguyentan <nguyentan@LAPTOP-PUU745D2>
2022-11-23 12:27:14 +0700

    remove triggerOnce

--- components/Skills/Skill.tsx
+++ components/Skills/Skill.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 const Skill = (props: Props) => {
   return (
-    <Fade cascade triggerOnce direction="up" delay={props.delay ? props.delay : 0}>
+    <Fade cascade direction="up" delay={props.delay ? props.delay : 0}>
       <div className="flex flex-col items-start">
         <div className="mb-6 p-5 rounded-md bg-slate-200 text-black hover:bg-black hover:text-white duration-200 ease-in-out">{props.icon}</div>
         <h3 className="font-heading text-xl mb-3">{props.title}</h3>
